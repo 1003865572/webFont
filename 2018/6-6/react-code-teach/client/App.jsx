9d46ddd61009e15e073e5c4485c5990ead9fa7bb
1003865572 <1003865572@qq.com>
2018-06-07 22:32:52 +0800

    6-7

--- 2018/6-6/react-code-teach/client/App.jsx
+++ 2018/6-6/react-code-teach/client/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 class App extends React.Component {
     render () {
-        return (<div>this is a app</div>)
+        return (
+            <div>this is a app 321321  </div>
+        )
     }
 }
 
