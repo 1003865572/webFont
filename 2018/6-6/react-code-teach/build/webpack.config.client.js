021ddc83d60128dd4fa0e439d849f45d7c805073
1003865572 <1003865572@qq.com>
2018-06-18 17:49:44 +0800

    6-18

--- 2018/6-6/react-code-teach/build/webpack.config.client.js
+++ 2018/6-6/react-code-teach/build/webpack.config.client.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpackMerge = require('webpack-merge')
 const baseConfig = require('./webpack.base')
+const NameALlModulesPlugin = require('name-all-modules-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -49,6 +50,57 @@ if (isDev) {
     }
   }
   config.plugins.push(new webpack.HotModuleReplacementPlugin())
+} else {
+  config.entry = {
+    app: path.join(__dirname, '../client/app.js'),
+    vendor: [
+      'react',
+      'react-dom',
+      'react-router',
+      'react-router-dom',
+      'mobx',
+      'mobx-react',
+      'axios',
+      'query-string',
+      'dateformat',
+      'marked'
+    ]
+  }
+  config.output.filename = '[name].[chunkhash].js'
+
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin()) // 压缩代码
+  config.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({ // 把 entry 中的 vendor 单独打包
+      name: 'vendor'
+    })
+  )
+  config.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({ // webpack 每次打包都会生成一部分代码, 把这部分代码定义好
+      name: 'mainfest',
+      minChunks: Infinity
+    })
+  )
+  config.plugins.push(
+    new webpack.NamedModulesPlugin()
+  )
+  config.plugins.push(
+    new NameALlModulesPlugin()
+  )
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  )
+  config.plugins.push(
+    new webpack.NamedChunksPlugin(chunk => {
+      if (chunk.name) {
+        return chunk.name
+      }
+      return chunk.mapModules(m => path.relative(m.context, m.request)).join('_')
+    })
+  )
 }
 
 module.exports = config
